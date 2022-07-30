74445983af6130505ae266a15f61ecddb0cbd30e
mennaAFathy1014 <menna10awaden@gmail.com>
2022-07-30 22:50:38 +0200

    modified script

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,108 @@
+let arrayOfTasks = [];
+let input = document.querySelector('.toDO-input');
+let tasks = document.querySelector('.todo-list');
+let checkAll = document.querySelector('.checkAll');
 
+//check if there is tasks in local storage
+if(window.localStorage.getItem('tasks')){
+    arrayOfTasks = JSON.parse(window.localStorage.getItem('tasks'));
+    addElementsToPage(arrayOfTasks);
+}
+
+
+//click on element
+tasks.addEventListener('click',(e)=>{
+    //delete button
+    if(e.target.classList.contains('del')){
+        let id = e.target.parentElement.getAttribute('data-id');
+        deleteTask(id);
+    }
+    if(e.target.classList.contains('check')){
+        let id = e.target.parentElement.getAttribute('data-id');
+        completeTask(id);
+    }
+})
+
+
+input.addEventListener('keydown',function(e){
+    if(e.keyCode === 13 && input.value !==''){
+        let taskText = input.value;
+        addTasksToarray(taskText);
+        // addElementsToPage(arrayOfTasks);
+        input.value = '';
+    }
+})
+
+checkAll.addEventListener('click',function(){
+    arrayOfTasks.forEach((task)=>{
+        task.completed = !task.completed;
+    }
+    )
+    addDataToLocalStorage(arrayOfTasks);
+
+})
+
+function addTasksToarray(taskText){
+    let task = {
+        id:Date.now(),
+        title:taskText,
+        completed:false
+    }
+    // add task to array
+    arrayOfTasks.push(task);
+    //add task to page
+    addElementsToPage(arrayOfTasks);
+    //add to local storage
+    addDataToLocalStorage(arrayOfTasks);
+}
+function addDataToLocalStorage(arrayOfTasks){
+    window.localStorage.setItem('tasks',JSON.stringify(arrayOfTasks));
+    addElementsToPage(arrayOfTasks);
+    // console.log(window.localStorage.getItem('tasks'));
+}
+function addElementsToPage(arrayOfTasks){
+    tasks.innerHTML = '';
+    //loop through array of tasks
+    arrayOfTasks.forEach(function(task){
+        //create main div 
+        let div = document.createElement('div');
+        div.className='item';
+        div.setAttribute('data-id',task.id);
+        //create checkbox
+        let check = document.createElement('button');
+        check.className = 'check';
+        div.appendChild(check);
+
+        //add text
+        let text = document.createElement('span');
+        text.className = 'text';
+        text.innerHTML = task.title;
+        div.appendChild(text);
+        if(task.completed){
+            text.className += ' completed';
+            check.innerHTML = '&check;';
+        }
+        // delete button
+        let deleteButton = document.createElement('button');
+        deleteButton.className = 'del';
+        deleteButton.innerHTML = '&times;';
+        div.appendChild(deleteButton);
+        tasks.appendChild(div);
+
+    });
+}
+//delete task
+function deleteTask(id){
+    arrayOfTasks = arrayOfTasks.filter((task)=> task.id != id);
+    addDataToLocalStorage(arrayOfTasks);
+}
+//complete task
+function completeTask(id){
+    arrayOfTasks.forEach((task)=>{
+        if(task.id == id){
+            task.completed = !task.completed;
+        }
+    }
+    )
+    addDataToLocalStorage(arrayOfTasks);
+}
